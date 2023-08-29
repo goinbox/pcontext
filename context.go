350e978332ae8a2d63aa7bee1a2b7633f0b3f999
gibsonli <gibsonli@tencent.com>
2023-08-29 11:02:41 +0800

    change interface func

--- context.go
+++ context.go
@@ -9,27 +9,20 @@ import (
 type Context interface {
 	context.Context
 
-	TraceID() string
 	Logger() golog.Logger
 }
 
 type simpleContext struct {
 	context.Context
 
-	tid    string
 	logger golog.Logger
 }
 
-func NewSimpleContext(tid string, logger golog.Logger) Context {
+func NewSimpleContext(logger golog.Logger) Context {
 	return &simpleContext{
 		Context: context.Background(),
-		tid:     tid,
 		logger:  logger,
 	}
-}
-
-func (s *simpleContext) TraceID() string {
-	return s.tid
 }
 
 func (s *simpleContext) Logger() golog.Logger {
